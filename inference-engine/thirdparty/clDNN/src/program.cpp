4073af60e9981d55e14e3b0718d7f0198e0c11b0
Sergey, Shlyapnikov <sergey.shlyapnikov@intel.com>
2021-03-09 19:35:57 +0300

    WIP: [IE CLDNN] Disable fsv16 optimized format for MVN in float models

--- inference-engine/thirdparty/clDNN/src/program.cpp
+++ inference-engine/thirdparty/clDNN/src/program.cpp
@@ -1231,8 +1231,7 @@ void program_impl::set_layout_optimizer_attributes(layout_optimizer& lo) {
             prim.type() != cldnn::mutable_data::type_id() &&
             prim.type() != cldnn::reduce::type_id() &&
             prim.type() != cldnn::strided_slice::type_id() &&
-            prim.type() != cldnn::region_yolo::type_id() &&
-            prim.type() != cldnn::mvn::type_id())
+            prim.type() != cldnn::region_yolo::type_id())
             can_use_fsv16 = false;
 
         if (prim.type() == cldnn::quantize::type_id() &&
