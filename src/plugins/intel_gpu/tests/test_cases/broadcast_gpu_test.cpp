796146387a592355b7cd82bf85bea65ca240d029
Sergey Shlyapnikov <sergey.shlyapnikov@intel.com>
2022-08-02 15:38:20 +0900

    [GPU] Align broadcast with nGraph operation

--- src/plugins/intel_gpu/tests/test_cases/broadcast_gpu_test.cpp
+++ src/plugins/intel_gpu/tests/test_cases/broadcast_gpu_test.cpp
@@ -1434,7 +1434,7 @@ TEST(broadcast_gpu, basic_error_wrong_b_axes_size) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {2, 3, 4, 5}, {0, 1, 2, 3, 4}));
+    topology.add(broadcast("output", "input", tensor{2, 3, 4, 5}, {0, 1, 2, 3, 4}));
 
     std::string msg_to_find = "Incorrect parameters configuration: broadcast_axes size should be less or equal 4.";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
@@ -1447,7 +1447,7 @@ TEST(broadcast_gpu, basic_error_wrong_b_axis_value) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {2, 3, 4, 5}, {0, 4}));
+    topology.add(broadcast("output", "input", tensor{2, 3, 4, 5}, {0, 4}));
 
     std::string msg_to_find = "Incorrect parameters configuration: broadcast_axes index should be within broadcast_sizes range.";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
@@ -1460,7 +1460,7 @@ TEST(broadcast_gpu, basic_error_duplicate_b_axis_values) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {2, 3, 4, 5}, {0, 1, 1}));
+    topology.add(broadcast("output", "input", tensor{2, 3, 4, 5}, {0, 1, 1}));
 
     std::string msg_to_find = "Incorrect parameters configuration: Duplicate axes numbers was found in broadcast_axes.";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
@@ -1473,7 +1473,7 @@ TEST(broadcast_gpu, basic_error_wrong_input_dimension_0) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {2, 3, 4, 5}, {1}));
+    topology.add(broadcast("output", "input", tensor{2, 3, 4, 5}, {1}));
 
     std::string msg_to_find = "Input size on dimension number 0(=2) is not equal to: (=1)";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
@@ -1486,7 +1486,7 @@ TEST(broadcast_gpu, basic_error_not_dividable_2x3x4x5_to_3x3x4x5) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {3, 3, 4, 5}, {}));
+    topology.add(broadcast("output", "input", tensor{3, 3, 4, 5}, {}));
 
     std::string msg_to_find = "Invalid broadcast size: not dividable by input size";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
@@ -1499,7 +1499,7 @@ TEST(broadcast_gpu, basic_error_not_dividable_3_to_2x3x4x5_w_b_axes_0x1x3) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {2, 3, 4, 5}, {0, 1, 3}));
+    topology.add(broadcast("output", "input", tensor{2, 3, 4, 5}, {0, 1, 3}));
 
     std::string msg_to_find = "Invalid broadcast size: not dividable by input size";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
@@ -1512,7 +1512,7 @@ TEST(broadcast_gpu, basic_error_not_dividable_4x5_to_3x4x5_w_b_axes_1) {
 
     topology topology;
     topology.add(input_layout("input", input->get_layout()));
-    topology.add(broadcast("output", "input", {2, 3, 4, 5}, {1}));
+    topology.add(broadcast("output", "input", tensor{2, 3, 4, 5}, {1}));
 
     std::string msg_to_find = "Invalid broadcast size: not dividable by input size";
     EXPECT_ANY_THROW(check_exception_massage(engine, topology, msg_to_find));
