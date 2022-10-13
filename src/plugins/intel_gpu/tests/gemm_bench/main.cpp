e89d2e7b0ad533f396f18b3c7380b6ad9ee16b18
Sergey Shlyapnikov <sergey.shlyapnikov@intel.com>
2022-10-13 14:36:22 +0000

    [GPU] Add parameterization support

--- src/plugins/intel_gpu/tests/gemm_bench/main.cpp
+++ src/plugins/intel_gpu/tests/gemm_bench/main.cpp
@@ -5,12 +5,21 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 #include "test_utils.h"
+#include "gflags/gflags.h"
 
 #include <intel_gpu/primitives/input_layout.hpp>
 #include <intel_gpu/primitives/gemm.hpp>
 #include <intel_gpu/primitives/fully_connected.hpp>
 
 #include <cstddef>
+
+DEFINE_string(mnk, "", "");
+DEFINE_string(kernel, "", "");
+DEFINE_string(dt, "", "");
+DEFINE_bool(use_onednn, false, "");
+DEFINE_bool(gemm, false, "");
+DEFINE_bool(fc, false, "");
+DEFINE_bool(profiling, true, "");
 
 using namespace cldnn;
 using namespace ::tests;
@@ -27,13 +36,27 @@ enum class matmul_type {
 std::ostream& operator<<(std::ostream& stream,
                          const matmul_type& type) {
     if (type == matmul_type::gemm)
-        stream << "gemm";
+        stream << "GEMM";
     else if (type == matmul_type::fc)
-        stream << "fc";
+        stream << "FC";
     else
         throw std::runtime_error("Unsupported matmul type");
     return stream;
- }
+}
+
+std::vector<int> parse_mnk_string(std::string mnk_string) {
+    std::vector<int> mnk_vals;
+    std::stringstream ss(mnk_string);
+    std::istream_iterator<std::string> ss_itr(ss);
+
+    for (auto i = ss_itr; i != std::istream_iterator<std::string>(); i++)
+        mnk_vals.push_back(std::stoi(*i));
+
+    if (mnk_vals.size() != 3)
+        throw std::runtime_error("Incorrect MNK sizes specified");
+
+    return mnk_vals;
+}
 
 struct gemm_params;
 
@@ -62,34 +85,63 @@ struct gemm_params {
     std::string kernel_name;
 };
 
+#define DEFAULT_PARAMS 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, false, false, \
+1.0f, 0.0f, { -128, 127, 1 }, { 0, 255, 1 }, { -10, 10, 8 }
+
 #define CASE_0 256, 256, 256, 1, 2, 1, 1, 1, 1, 1, 2, false, false, \
 1.5f, 2.0f, { -128, 127, 1 }, { 0, 255, 1 }, { -10, 10, 8 }
 
 #define CASE_1 512, 1024, 256, 1, 1, 1, 1, 1, 1, 1, 1, false, false, \
 1.0f, 1.0f, { -1, 1, 1 }, { -1, 1, 1 }, { -1, 1, 1 }
 
-int main() {
-    execute<uint8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_0, ""}, matmul_type::gemm);
-    execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_0, ""}, matmul_type::gemm);
-    execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_0, "gemm_ref"}, matmul_type::gemm);
-    execute<int8_t, int8_t, float, float>(gemm_params{CASE_0, "gemm_ref"}, matmul_type::gemm);
-    execute<int8_t, int8_t, float, float>(gemm_params{CASE_0, "gemm_mmad_int8_slm"}, matmul_type::gemm);
+int main(int argc, char* argv[]) {
+    gflags::ParseCommandLineNonHelpFlags(&argc, &argv, true);
 
-    execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "gemm_ref"}, matmul_type::gemm);
-    execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "gemm_tiled_opt"}, matmul_type::gemm);
+    if (FLAGS_gemm == FLAGS_fc)
+        throw std::runtime_error("Please specify correct primitive type (-fc/-gemm)");
 
-    execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "fully_connected_gpu_bf_tiled"}, matmul_type::fc);
-    execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "fully_connected_gpu_bfyx_ref"}, matmul_type::fc);
+    if (FLAGS_dt.empty())
+        throw std::runtime_error("Please specify data type (i8/f16)");
 
-    execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_1, "fully_connected_gpu_imad"}, matmul_type::fc);
-    execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_1, "fully_connected_gpu_bfyx_ref"}, matmul_type::fc);
+    matmul_type matmul_t = FLAGS_gemm ? matmul_type::gemm : matmul_type::fc;
+
+    auto mnk_vals = parse_mnk_string(FLAGS_mnk);
+
+    gemm_params test_params{DEFAULT_PARAMS, FLAGS_kernel};
+    test_params.m_size = mnk_vals[0];
+    test_params.n_size = mnk_vals[1];
+    test_params.k_size = mnk_vals[2];
+
+    if (FLAGS_dt == "fp16" || FLAGS_dt == "f16") {
+        execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(test_params, matmul_t);
+    } else if (FLAGS_dt == "int8" || FLAGS_dt == "i8") {
+        execute<int8_t, int8_t, int8_t, int8_t>(test_params, matmul_t);
+    } else {
+        throw std::runtime_error("Unsupported data type: " + FLAGS_dt);
+    }
+
+    // execute<uint8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_0, ""}, matmul_type::gemm);
+    // execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_0, ""}, matmul_type::gemm);
+    // execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_0, "gemm_ref"}, matmul_type::gemm);
+    // execute<int8_t, int8_t, float, float>(gemm_params{CASE_0, "gemm_ref"}, matmul_type::gemm);
+    // execute<int8_t, int8_t, float, float>(gemm_params{CASE_0, "gemm_mmad_int8_slm"}, matmul_type::gemm);
+
+    // execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "gemm_ref"}, matmul_type::gemm);
+    // execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "gemm_tiled_opt"}, matmul_type::gemm);
+
+    // execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "fully_connected_gpu_bf_tiled"}, matmul_type::fc);
+    // execute<FLOAT16, FLOAT16, FLOAT16, FLOAT16>(gemm_params{CASE_1, "fully_connected_gpu_bfyx_ref"}, matmul_type::fc);
+
+    // execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_1, "fully_connected_gpu_imad"}, matmul_type::fc);
+    // execute<int8_t, int8_t, int8_t, int8_t>(gemm_params{CASE_1, "fully_connected_gpu_bfyx_ref"}, matmul_type::fc);
     return 0;
 }
 
 template <typename input0_type, typename input1_type, typename input2_type, typename output_type>
 void execute(gemm_params p, matmul_type prim_type) {
-    const bool enable_profiling = true;
-    const bool use_onednn = false;
+    const bool enable_profiling = FLAGS_profiling;
+    const bool use_onednn = FLAGS_use_onednn;
+    const bool print_info = true;
 
     static std::shared_ptr<cldnn::engine> engine = nullptr;
     if (!engine) {
@@ -160,9 +212,15 @@ void execute(gemm_params p, matmul_type prim_type) {
             topology.add(gemm("matmul_prim", { "input0", "input1" }, output_dt, p.transpose_input0, p.transpose_input1, p.alpha, p.beta));
         }
         topology.add(reorder("reorder_bfyx", "matmul_prim", format::bfyx, data_types::f32));
+
+        if (print_info) {
+            std::cout << "M=" << p.m_size << " K=" << p.k_size << " N=" << p.n_size <<  std::endl;
+            std::cout << "Input0: " << input0_mem->get_layout().to_short_string() << (p.transpose_input0 ? " - Transposed" : "") <<  std::endl;
+            std::cout << "Input1: " << input1_mem->get_layout().to_short_string() << (p.transpose_input1 ? " - Transposed" : "") <<  std::endl;
+        }
     } else if (prim_type == matmul_type::fc) {
         auto input0_size = tensor((int)p.m_size, (int)p.k_size, 1, 1);
-        VVVVF<input0_type> input0_data = generate_random_4d<input0_type>(p.b0_num, p.f0_num, 1, 1, p.range0[0], p.range0[1], p.range0[2]);
+        VVVVF<input0_type> input0_data = generate_random_4d<input0_type>(p.m_size, p.k_size, 1, 1, p.range0[0], p.range0[1], p.range0[2]);
         auto input0_data_bfyx = flatten_4d(format::bfyx, input0_data);
         input0_mem = engine->allocate_memory({ input0_dt, format::bfyx, input0_size });
         set_values(input0_mem, input0_data_bfyx);
@@ -177,6 +235,12 @@ void execute(gemm_params p, matmul_type prim_type) {
         topology.add(data("weights", input1_mem));
         topology.add(fully_connected("matmul_prim", "input0", "weights", "", output_dt));
         topology.add(reorder("reorder_bfyx", "matmul_prim", format::bfyx, data_types::f32));
+
+        if (print_info) {
+            std::cout << "M=" << p.m_size << " K=" << p.k_size << " N=" << p.n_size <<  std::endl;
+            std::cout << "Input0: " << input0_mem->get_layout().to_short_string() << (p.transpose_input0 ? " - Transposed" : "") <<  std::endl;
+            std::cout << "Weights: " << input1_mem->get_layout().to_short_string() << (p.transpose_input1 ? " - Transposed" : "") << " (Weights may be reordered during preprocessing)" <<  std::endl;
+        }
     } else {
         throw std::runtime_error("Unsupported primitive type for benchmarking");
     }
@@ -238,5 +302,14 @@ void execute(gemm_params p, matmul_type prim_type) {
             total_time_us += calc_time(gemm->first, gemm->second);
         }
         std::cout << prim_type << " kernel (" << kernel_name << ") time is " << total_time_us / iteration_num << "us" << std::endl;
+    } else {
+        auto executed_primitives = network.get_executed_primitives();
+        auto gemm = executed_primitives.find("matmul_prim") != executed_primitives.end() ? executed_primitives.find("matmul_prim") :
+                                                                                           executed_primitives.find("reorder_bfyx");
+        if (gemm == executed_primitives.end())
+            throw std::runtime_error("Couldn't find profiling info for requested primitive\n");
+
+        kernel_name = network.get_implementation_info(gemm->first);
+        std::cout << prim_type << " kernel (" << kernel_name << ")" << std::endl;
     }
 }
